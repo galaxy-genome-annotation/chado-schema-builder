1ecf7cacad4ac88f107d353652b1ea825e31fd4c
Eric Rasche <rasche.eric@gmail.com>
2016-08-12 12:54:43 +0000

    fix filenames

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ make
 make install
 make load_schema
 make prepdb
-pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/${PKG}-${VERSION}-no-onto.sql"
+pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/chado-${VERSION}-no-onto.sql"
 echo "1,2,3,4,5,6" | make ontologies
-pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/${PKG}-${VERSION}.sql"
+pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/chado-${VERSION}.sql"
 psql -h localhost -p 5432 -U postgres -c 'ALTER SCHEMA public RENAME TO chado'
-pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/${PKG}-${VERSION}-tripal.sql"
+pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/chado-${VERSION}-tripal.sql"
 
-gzip < "/host/${PKG}-${VERSION}-no-onto.sql" > "/host/${PKG}-${VERSION}-no-onto.sql.gz"
-gzip < "/host/${PKG}-${VERSION}.sql"         > "/host/${PKG}-${VERSION}.sql.gz"
-gzip < "/host/${PKG}-${VERSION}-tripal.sql"  > "/host/${PKG}-${VERSION}-tripal.sql.gz"
+gzip < "/host/chado-${VERSION}-no-onto.sql" > "/host/chado-${VERSION}-no-onto.sql.gz"
+gzip < "/host/chado-${VERSION}.sql"         > "/host/chado-${VERSION}.sql.gz"
+gzip < "/host/chado-${VERSION}-tripal.sql"  > "/host/chado-${VERSION}-tripal.sql.gz"

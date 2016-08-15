9472d80d989da6555cbe8855c0b3601089e5203c
Eric Rasche <rasche.eric@gmail.com>
2016-08-15 19:02:43 +0000

    Magic command from @scottcain

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 wget --quiet "https://github.com/GMOD/Chado/archive/${BRANCH}.tar.gz"
 tar xfz "${BRANCH}.tar.gz"
 cd "Chado-${BRANCH}/chado/" || exit;
@@ -13,7 +14,9 @@ make install
 make load_schema
 make prepdb
 pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/chado-${VERSION}-no-onto.sql"
-echo "1,2,3,4,5,6" | make ontologies
+echo "1" | make ontologies
+psql -h localhost -p 5432 -U postgres -c "UPDATE cvterm SET cv_id = 7 WHERE cv_id IN (SELECT cv_id FROM cv WHERE name='ro')"
+echo "2,3,4,5,6" | make ontologies
 pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/chado-${VERSION}.sql"
 psql -h localhost -p 5432 -U postgres -c 'ALTER SCHEMA public RENAME TO chado'
 pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/chado-${VERSION}-tripal.sql"
@@ -21,3 +24,6 @@ pg_dump -h localhost -p 5432 -U postgres --no-owner --no-acl postgres > "/host/c
 gzip < "/host/chado-${VERSION}-no-onto.sql" > "/host/chado-${VERSION}-no-onto.sql.gz"
 gzip < "/host/chado-${VERSION}.sql"         > "/host/chado-${VERSION}.sql.gz"
 gzip < "/host/chado-${VERSION}-tripal.sql"  > "/host/chado-${VERSION}-tripal.sql.gz"
+
+echo "The schema build has completed. The container will now intentionally crash"
+exit 1;

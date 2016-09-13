6db739193fc2f15892f96ee249ebaf1494e441fc
Eric Rasche <rasche.eric@gmail.com>
2016-09-13 20:06:29 +0000

    Automatically make releases

---
+++ .ci/upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+OUTPUT_DIRECTORY="${OUTPUT_DIRECTORY:-$(pwd)/output}";
+if [ ! -e ghr  ]; then
+	wget "https://github.com/tcnksm/ghr/releases/download/v0.4.0/ghr_v0.4.0_linux_amd64.zip";
+	unzip ghr_v0.4.0_linux_amd64.zip;
+	chmod +x ghr
+fi
+
+CHADO_VERSION=$(find ${OUTPUT_DIRECTORY}/chado-*.sql | grep -o 'chado-[/0-9.]*.sql' | sed 's/chado-//g;s/\.sql//g;')
+./ghr -u erasche -r chado-schema-builder "${CHADO_VERSION}" "${OUTPUT_DIRECTORY}"
